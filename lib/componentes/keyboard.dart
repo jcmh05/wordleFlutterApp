35ad621a9d20bb770e1911a7be1dda634589c0bb
Juan Montilla <jcmh0005@red.ujaen.es>
2023-03-28 20:13:00 +0200

    AvanceTeclado

--- lib/componentes/keyboard.dart
+++ lib/componentes/keyboard.dart
@@ -2,72 +2,53 @@ import 'package:flutter/material.dart';
 import './componentes.dart';
 
 class Keyboard extends StatefulWidget{
-  const Keyboard({Key? key}) : super( key: key);
+  final Function(String) TeclaPulsada;
+  const Keyboard({Key? key, required this.TeclaPulsada}) : super( key: key);
 
   @override
-  State<Keyboard> createState() => _KeyboardState();
+  State<Keyboard> createState() => _KeyboardState(TeclaPulsada: TeclaPulsada);
 }
 
 class _KeyboardState extends State<Keyboard> {
+  //Función anónima que informa a la pantalla_juego de que tecla ha sido pulsada
+  final Function(String) TeclaPulsada;
+  // Teclas máximas que puede haber por fila en el teclado
+  final teclasPorFila = 10;
+  // Espacio entre las distintas filas del teclado
+  final separacion = 15.0;
+  // Teclas correspondientes al teclado en español
+  final tecladoEspanol = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Ñ','Z','X','C','V','B','N','M','BORRAR'];
+
+  _KeyboardState({required this.TeclaPulsada});
 
   @override void initState() {
     // TODO: implement initState
     super.initState();
   }
 
+
+  List<Widget> columnasTeclas(){
+    final columnas = <Widget>[];
+    int letra = 0;
+    for(int i=0; i<3; i++){
+      columnas.add(SizedBox(height: separacion));//Espacio entre una fila de teclas y otra
+      final fila = <Widget>[];
+      for(int j=0; j<teclasPorFila; j++){
+        if( letra != tecladoEspanol.length){
+          fila.add(KeyButton(letter: tecladoEspanol[letra], TeclaPulsada: TeclaPulsada,));
+          letra++;
+        }
+      }
+      columnas.add(Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: fila,));
+    }
+
+    return columnas;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        SizedBox(height: 10),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            //Caja que contiene al botón y restringirá sus medidas
-            KeyButton(letter: 'Q'),
-            KeyButton(letter: 'W'),
-            KeyButton(letter: 'E'),
-            KeyButton(letter: 'R'),
-            KeyButton(letter: 'T'),
-            KeyButton(letter: 'Y'),
-            KeyButton(letter: 'U'),
-            KeyButton(letter: 'I'),
-            KeyButton(letter: 'O'),
-            KeyButton(letter: 'P'),
-          ],
-        ),
-        SizedBox(height: 10),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            //Caja que contiene al botón y restringirá sus medidas
-            KeyButton(letter: 'A'),
-            KeyButton(letter: 'S'),
-            KeyButton(letter: 'D'),
-            KeyButton(letter: 'F'),
-            KeyButton(letter: 'G'),
-            KeyButton(letter: 'H'),
-            KeyButton(letter: 'J'),
-            KeyButton(letter: 'K'),
-            KeyButton(letter: 'L'),
-            KeyButton(letter: 'Ñ'),
-          ],
-        ),
-        SizedBox(height: 10),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            //Caja que contiene al botón y restringirá sus medidas
-            KeyButton(letter: 'Z'),
-            KeyButton(letter: 'X'),
-            KeyButton(letter: 'C'),
-            KeyButton(letter: 'V'),
-            KeyButton(letter: 'B'),
-            KeyButton(letter: 'N'),
-            KeyButton(letter: 'M'),
-          ],
-        ),
-      ],
+      children: columnasTeclas(),
     );
   }
 
---
+++ lib/pantallas/pantalla_juego.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../componentes/componentes.dart';
+
+class PantallaJuego extends StatefulWidget{
+
+  const PantallaJuego({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _PantallaJuegoState();
+
+}
+
+class _PantallaJuegoState extends State<PantallaJuego> {
+
+  String teclasPulsadas = " ";
+
+
+  @override
+  Widget build( BuildContext context) {
+    final teclaPulsada = (String tecla) {
+      setState(() {
+        if( tecla == "BORRAR"){
+          teclasPulsadas = " ";
+        }else{
+          teclasPulsadas = teclasPulsadas + tecla + " ";
+        }
+      });
+    };
+
+    return Column(
+      children: [
+
+        //Prueba de teclado
+        SizedBox(height: 15.0),
+        Text('Teclas Pulsadas: ', style: TextStyle(fontSize: 24)),
+        Text(  teclasPulsadas,style: TextStyle(fontSize: 20)),
+
+        //Teclado
+        Keyboard(TeclaPulsada: teclaPulsada),
+      ],
+    );
+  }
+}
